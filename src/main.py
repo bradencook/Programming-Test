5b0ffead133a10fb70c594094622ff10005499dc
bradencook <bradencook98@gmail.com>
2022-07-14 08:40:31 -0600

    testing a sample of the data using print statements

--- src/main.py
+++ src/main.py
@@ -6,9 +6,30 @@ if __name__ == "__main__":
     # skip the first line
     streetProvo.readline()
 
-    dayTotals = []
+    dayTotals = {}
+    count = 0
     for line in streetProvo:
         line = line.split(",")
-        print(line[FIELD_VERIFIED])
+        if line[FIELD_VERIFIED] != "" and count < 30:
+            dateComplete = line[FIELD_VERIFIED].strip().split(" ")[0]
+            # make the dates sortable yyyy/mm/dd
+            rawDate = dateComplete.split("/")
+            if len(rawDate[0]) == 1:
+                rawDate[0] = "0" + rawDate[0]
+            if len(rawDate[1]) == 1:
+                rawDate[1] = "0" + rawDate[1]
+            dateComplete = rawDate[2] + "/" + rawDate[0] + "/" + rawDate[1]
+
+            print(dateComplete + "\t" + line[LENGTH_YDS])
+            count += 1
+            ydsTotal = float(line[LENGTH_YDS])
+            if dateComplete in dayTotals:
+                ydsTotal += float(dayTotals[dateComplete])
+            dayTotals.update({dateComplete: ydsTotal})
+        
+    print(dayTotals)
+    dates = list(dayTotals.keys())
+    dates.sort()
+    print(dates)
 
     streetProvo.close()
