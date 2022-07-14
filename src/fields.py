7cddf84e7507ab059acb9a746bec7156f9723df7
bradencook <bradencook98@gmail.com>
2022-07-14 02:15:20 -0600

    Started putting together the main program

--- src/fields.py
+++ src/fields.py
@@ -1,8 +1,10 @@
 if __name__ == '__main__':
     streetProvo = open("../StreetProvo.csv")
     fields = streetProvo.readline().split(',')
+    line1 = streetProvo.readline().split(',')
     file = open("../fields.txt", "w")
+    print("FIELD\tNAME\tEXAMPLE DATA", file=file)
     for i in range(len(fields)):
-        print(f"{i}   {fields[i]}", file=file)
+        print(f"{i}\t{fields[i]}\t\t\t{line1[i]}", file=file)
     streetProvo.close()
     file.close()
--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
+FIELD_VERIFIED = 112
+LENGTH_YDS = 115
 
+if __name__ == "__main__":
+    streetProvo = open("../StreetProvo.csv")
+    # skip the first line
+    streetProvo.readline()
+
+    dayTotals = []
+    for line in streetProvo:
+        line = line.split(",")
+        print(line[FIELD_VERIFIED])
+
+    streetProvo.close()
