8b2706c78c6957c7c532436085054ef490c63fa7
bradencook <bradencook98@gmail.com>
2022-07-13 23:31:43 -0600

    Organizing the repo and studying the data

---
+++ doc/signature.md
@@ -0,0 +1,5 @@
+#Signature
+
+| Date         | Time Spent | Events
+|--------------|------------|--------------------
+| July 13      | 1 hour     | Creating Github repo; Reviewing some of my code from school; studying the fields of the .csv file
--- src/fields.py
+++ src/fields.py
@@ -1,3 +1,8 @@
-# Braden Cook
-# CS1400 - A01
-# Assignment ??
+if __name__ == '__main__':
+    streetProvo = open("../StreetProvo.csv")
+    fields = streetProvo.readline().split(',')
+    file = open("../fields.txt", "w")
+    for i in range(len(fields)):
+        print(f"{i}   {fields[i]}", file=file)
+    streetProvo.close()
+    file.close()
